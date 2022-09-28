49651a43139126cd1a282939a02a829077887050
Glitch (glitch-hello-website) <none>
2022-09-28 00:45:02 +0000

    🚠🍜 Checkpoint ./.glitch-assets:270926/43 ./animations.js:270926/4819 ./index.html:270926/4965 ./style.css:270926/5436 ./app.js:270926/6745

---
+++ animations.js
@@ -0,0 +1,175 @@
+// You can change this, 
+// but will need to change swatch-holder's tile settings in CSS
+const SWATCH_SIZE = 300;
+
+let animations = [
+
+	//================================================
+	// An example
+	{
+		title: "splatter",
+		description: "Basic drawing and randomness. See how using the full spectrum, a partial spectrum, or two different spectrums, or driving it based on time, can affect how the art looks",
+		isActive: false,
+
+		/**
+		* TODO: Read this!
+		* Setup and draw both have a "p" parameter
+		* This is the P5 object. 
+		* 
+		* All of the built-in drawing tools from P5 
+		*  are methods on this object
+		* 
+		* If you use any P5 tutorials, usually you will have to 
+		*  add "p." in front of their commands
+		* e.g.  "rect(0,0,100,300)" => "p.rect(0,0,100,300)"
+		* 
+		* "t" is the seconds that this app has been open
+		* You can use that do drive location color, etc
+		*/
+
+		setup(p) {
+			p.background(0, 0, 0)
+		},
+
+		draw(p, t, pct) {
+			// Fun trick: make a semi-transparent background (opacity .02)
+			//  in order to have the older parts of the drawing "fade away"
+			// p.background(0, 0, 0, .02)
+
+			// Any color in the rainbow
+			let hue = Math.random()*360
+
+			// Use this line instead for just blue circles
+			// let hue = Math.random()*100 + 150 
+
+			// Ternary operator: there's a 30% chance of orange, 70% chance of green
+			// let hue = Math.random()*30 + (Math.random()<.3?20:170)
+			
+			// Use the time
+			// let hue = t*100
+
+			
+			let sat = 100
+			let brightness = 50
+			let opacity = Math.random()
+
+			p.noStroke()
+			p.fill(hue%360, sat, brightness, opacity)
+
+			let r = Math.random()*10 + 10
+			let x = Math.random()*p.width
+			let y = Math.random()*p.height
+			p.circle(x, y, r)
+		}	
+	},
+	
+
+	//================================================
+	// An example
+
+	{
+		title: "movement",
+		description: "How can you control movement? We can time to drive the animation, using functions like the sine wave and perlin noise",
+		isActive: false,
+
+		setup(p, t, pct) {
+			// Draw this once at the beginning
+			p.background(0, 0, 0)
+			
+
+		},
+
+		draw(p, t, pct) {
+
+			// The center of the swatch is at (p.width/2, p.height/2)
+			let x = p.width * (.5 + .5 * Math.sin(t))
+			let y = p.height * .5
+			// let y = p.height * (.5 + .5 * Math.sin(10*t))
+			let r = 100
+			
+			// Perlin noise
+			// A way to get smooth motion, but not predictable
+			// let x = p.width * p.noise(t)
+			// let y = p.height * p.noise(t + 100)
+			// let r = 100
+			
+
+			p.fill(100)
+			p.circle(x, y, r)
+			
+		}
+	},
+
+	//================================================
+	// An example
+
+	{
+		title: "polar coordinates",
+		description: "By using polar coordinates, you can get interesting radial patterns. Look at the difference between sine, noise, and constant radiuses",
+		isActive: false,
+
+		setup(p, t, pct) {
+			p.background(0, 0, 0, 0)
+
+			// You can also store information on the swatch
+			this.theta = 0
+
+		},
+		draw(p, t, pct) {
+			p.background(0, 0, 0, 0.02)
+      this.theta += .04
+
+			let centerX = p.width/2
+			let centerY = p.height/2
+
+			// let radius = 100
+			let radius = 100*Math.sin(t)
+			// let radius = 100*p.noise(t)
+      // let radius = 100*p.noise(t*10)
+
+			let x = radius*Math.cos(this.theta) + centerX
+			let y = radius*Math.sin(this.theta) + centerY
+			let r = 10
+
+			p.noStroke()
+			p.fill(100)
+			p.circle(x, y, r)
+		}
+	},
+
+	//================================================
+	// An example
+
+	{
+		title: "Looping",
+		description: "a red dot moving <p>another paragraph</p>",
+		isActive: true,
+
+		setup(p, t, pct) {
+			this.loopTime = 5
+
+		},
+		draw(p, t, pct) {
+			
+		}
+	},
+
+	//================================================
+	// An example
+
+	{
+		title: "Looping",
+		description: "a red dot moving <p>another paragraph</p>",
+		isActive: true,
+
+		setup(p, t, pct) {
+			
+
+		},
+		draw(p, t, pct) {
+			
+		}
+	},
+
+
+]
