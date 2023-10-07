a9252eaf89c8454534f01e32ba2b021c9e6c9559
Glitch (glitch-hello-website) <none>
2023-10-07 21:21:56 +0000

    🏞🏤 Checkpoint ./sketches.js:61753767/4454

--- sketches.js
+++ sketches.js
@@ -54,7 +54,9 @@ const sketches = [
     show: true,
 
     draw(p) {
-      p.background(0)
+      if (p.frameCount % 9 == 0) {
+        p.background(0)
+      }
       // Get time in SECONDS
       let t = p.millis() * 0.001;
       let hue = (t * 100) % 360;
@@ -70,57 +72,61 @@ const sketches = [
       
     },
   },
-  // below are kate's sketches
+  
   {
-    name: "my first sketch",
+    name: "sketch 2",
     show: true,
 
-    draw(p) {
-      // Get time in SECONDS
-      let t = p.millis() * 0.001;
+    setup(p) {
+      // Setup only happens once.
+      // We can use it to start off our canvas with a particular color
+      p.background(0, 0, 80);
+    },
 
-      // "draw" in p5 draws *each frame*
-      // So each frame, we draw a new circle
-      // in a random place, in a random color
+    draw(p) {
+      // Each frame, draw a light gray background
+      // p.background(0, 0, 80)
+      // Or a semi transparent background (NOTE TRY THIS IT'S COOL)
+      p.background(0, 0, 80, 0.1);
+      // Or no background? Try commenting these background lines
+      // on and off and see how they change
+
+      let t = p.frameCount * 0.05;
+
+      // We can use "parametric equations" to drive movment over time
+      // If we have one "t" value, and equations for x and y that depend on it
+      // then as we increase t, those equations will draw a path over time
+
+      // Some Khan videos on parametric curves
+      // https://www.youtube.com/watch?v=m6c6dlmUT1c
+      // https://www.youtube.com/watch?v=bb4bSCjlFAw
+
+      // You can make good art with them, especially with *polar* curves
+      // https://www.youtube.com/watch?v=Y2T31OQ-cWA
+
+      // You can also change the speed by
+      //multiplying by it by different numbers
+      // t *= 4
+      // t *= .4
+
+      let x = p.width * (0.5 + 0.5 * Math.sin(t * 2));
+      let y = p.width * (0.5 + 0.5 * Math.cos(t * 1.71));
 
       // If we save a color for the hue,
       // we can use it for both the fill and the stroke
       // so we can have a circle with an outline of a darker color
       // which looks nice
-      // p.background(330, 100, 50);
-      // Set the colors
-      // The parameters are hue, saturation, and lightness, from 0-100
-      // 50% lightness is medium,
-      // if you go to 100 it becomes white
-      // if you go to 0, it becomes black
-      // Here we set a maximum-saturated fill, with a darker stroke
-      // let hue = Math.random() * 360;
-      // Set hue to some function of the current time
-      let hue = (t * 100) % 360;
+      let hue = (t * 50) % 360;
+      let circleRadius = 60 * p.noise(t);
       p.strokeWeight(5);
       p.stroke(hue, 100, 20);
       p.fill(hue, 100, 50);
-      // p.circle(100, 100, 50);
-
-      // Want more about color spaces?
-      // "Your Colors Suck (it's not your fault)" (37 min)
-      // https://www.youtube.com/watch?v=fv-wlo8yVhk&t=1793s
-
-      // 		// What if we set a less-saturated fill?
-      // 		// p.fill(hue, 10, 50)
-
-      // 		// Or a lighter fill?
-      // 		// p.fill(hue, 100, 90)
-
-      // Give me a radius between 20 and 60
-      let circleRadius = Math.random() * 40 + 20;
-      let x = Math.random() * p.width;
-      let y = Math.random() * p.height;
-
       p.circle(x, y, circleRadius);
-      p.rect(x, y, circleRadius, circleRadius);
     },
   },
+  
+  // below are kate's sketches
+  
   {
     name: "my first animating sketch",
     show: false,
